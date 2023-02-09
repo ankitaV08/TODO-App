d159ad64592c1a92f1226c93055816fef0f5d2b8
ankitaV08 <50579235+ankitaV08@users.noreply.github.com>
2023-02-09 23:49:12 +0530

    added todoApp folder

---
+++ todoApp/app.py
@@ -0,0 +1,38 @@
+
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:1234@localhost:5432/todoapp'
+db = SQLAlchemy(app)
+
+app.app_context().push()
+
+class Todo(db.Model):
+    __tablename__ = 'todos'
+    id = db.Column(db.Integer, primary_key=True)
+    description = db.Column(db.String(), nullable=False)
+    
+    def __repr__(self):
+        return f'<Todo ID: {self.id}, description: {self.description}>'
+
+db.create_all()
+
+@app.route('/todos/create', methods = ['POST'])
+def create_todo():
+    description = request.form.get('description', '')
+    todo = Todo(description=description)
+    db.session.add(todo)
+    db.session.commit()
+    return redirect(url_for('index'))
+
+@app.route('/')
+def index():
+  return render_template('index.html', data=Todo.query.all())
+
+
+#always include this at the bottom of your code (port 3000 is only necessary in workspaces)
+
+if __name__ == '__main__':
+   app.run(host="0.0.0.0", port=3000)
+   app.debug = True
